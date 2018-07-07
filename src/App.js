9b45505a6bbf6e24ca4c447f578bb36fbe56865a
Regina <rilopatin@gmail.com>
2018-07-07 22:05:51 +0300

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+
 import styled from 'styled-components';
 
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
 
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+const HeagerWraper = styled.header`
+  width:100%;
+  height:49px;
+  background-color: #242424;
+  opacity:0.8;
+`
+const MenuWraper = styled.div`
+  width:89px;
+  padding-top:20px;
+`
+const MainWraper = styled.main`
+height:600px;
+padding-top:130px;
+background: url(${url}) no-repeat;
+background-size: cover;
+`
 class App extends Component {
-  render() {
-    return (
-        <div className={"wrapper"}>
-            <h1>Hello World!</h1>
-            <SayFullName name="Regina1" surname="Lopatin" link="https://vk.com"/>
-            <SayFullName name="Regina2" surname="Lopatin" link="https://vk.com"/>
-            <SayFullName name="Regina3" surname="Lopatin" link="https://vk.com"/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <HeagerWraper>
+                    <Grid>
+                        <Header />
+                    </Grid>
+                </HeagerWraper>
+                <MenuWraper>
+                    <Grid>
+                        <Menu />
+                    </Grid>
+                </MenuWraper>
+                <MainWraper>
+                    <Grid>
+                        <Main />
+                    </Grid>
+                </MainWraper>
+
+            </div>
+        );
+    }
 }
 
-function SayFullName(props) {
-    return (
-        <div>
-            <h1>Мое имя {props.name}, фамилия - {props.surname}</h1>
-            <a href = {props.link}>Ссылка на мой профиль</a>
-        </div>
-    )
-}
+
+
 
 export default App;
